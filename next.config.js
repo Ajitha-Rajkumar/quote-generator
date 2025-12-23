4ee1f4764718f3ebec6c58d012a47be2e443e5b4
Ajitha Rajkumar <ajitha.rajkumar@example.com>
2025-12-23 13:16:18 +0530

    Fix GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/quote-generator',
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
